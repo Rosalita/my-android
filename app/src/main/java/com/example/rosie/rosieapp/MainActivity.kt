6cabce26b9a7e2dc8f4fbed8daaaae9aca9615ab
Rosie Hamilton <enchantrix@gmail.com>
2018-02-02 17:02:26 +0000

    now sends text from one view to another

--- app/src/main/java/com/example/rosie/rosieapp/MainActivity.kt
+++ app/src/main/java/com/example/rosie/rosieapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.rosie.rosieapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.rosie.MESSAGE"
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +17,11 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
