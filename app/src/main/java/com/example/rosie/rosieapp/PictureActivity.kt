34256a1e65de4f772528ef478d0578f0d4c05ebf
Rosie Hamilton <enchantrix@gmail.com>
2018-02-15 15:10:55 +0000

    set up new activity for pictures

---
+++ app/src/main/java/com/example/rosie/rosieapp/PictureActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rosie.rosieapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by rosie on 15/02/2018.
+ */
+class TakePictureActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.picture_page)
+    }
+}
